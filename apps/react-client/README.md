8e51eed0622880705887f163fabd50021df56a94
Abir Zennir <abirzennir3@gmail.com>
2025-11-03 13:36:40 +0100

    Add real source code (removed embedded repos)

---
+++ apps/react-client/README.md
@@ -0,0 +1,4 @@
+# Client-React
+https://github.com/user-attachments/assets/1e598d93-74ea-4e46-9e6f-fcf6dbd63543
+
+
---
+++ apps/react-client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CompteForm from './components/CompteForm';
+import CompteList from './components/CompteList';
+
+export default function App() {
+  return (
+    <div>
+      <CompteForm />
+      <CompteList />
+    </div>
+  );
+}
